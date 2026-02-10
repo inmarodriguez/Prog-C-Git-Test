c34fe4d78fee97b3691d8094bdbf924e4f72a9d4
Inma <inmarodriguez@ub.edu>
2026-02-10 13:39:55 +0100

    resolt

--- main.c
+++ main.c
@@ -11,12 +11,8 @@ int main(){
   assert(suma(-1,1)==0);
   assert(suma(10,10)==20);  
 
-<<<<<<< HEAD
 
-  printf("Editat en local. EXIT: Tots els tests han passat correctament!\n");
-=======
   printf(" Editat en local. EXIT: Tots els tests han passat correctament!\n");
->>>>>>> db107aa (Canvi local)
   return 0;
 
 }
