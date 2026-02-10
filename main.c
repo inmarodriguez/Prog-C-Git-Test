0352ace2a844bd94f2ab5ae228a76dd2aafec7a7
Inma <inmarodriguez@ub.edu>
2026-02-10 13:06:44 +0100

    asserts

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <assert.h>
+
+int suma (int a, int b){
+  return a+b;
+}
 
 int main(){
 
-  printf("Hola, Mon des de Git en C");
+  assert(suma(2,3)==5);
+  assert(suma(-1,1)==0);
+  assert(suma(10,10)==20);  
+
+  printf("EXIT: Tots els tests han passat correctament!\n");
   return 0;
 
 }
